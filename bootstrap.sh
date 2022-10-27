76d8feba91563afad96afccc092518a6cff9e185
Michael Markevich <97518523+mm1024@users.noreply.github.com>
2022-10-27 15:57:03 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/bash
 
 DHIS2_TOOLS_DIR="/opt/dhis2-server-tools"
+DHIS2_EMAIL="security@dhis2.org"
 
 # Update the environment
 apt-get -qq update
 apt-get -yqq dist-upgrade
 
-# Configure the hostname
+# Fetch the FQDN
 apt-get install -yqq curl jq
 DHIS2_SPECIMEN_HOST=`curl -s http://169.254.169.254/openstack/latest/meta_data.json | jq -j .name`
 
+# Set the FQDN
 echo -e "127.0.0.1\tlocalhost\n127.0.1.1\t$DHIS2_SPECIMEN_HOST `hostname`\n\n::1\tlocalhost ip6-localhost ip6-loopback\nff02::1\tip6-allnodes\nff02::2\tip6-allrouters" > /etc/hosts
-exit
 
 # We install and configure a default OS environment for the DHIS2 instance
 apt-get install -yqq dialog git sed software-properties-common unattended-upgrades
