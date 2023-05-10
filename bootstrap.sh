5d9edb068ab9fc2fd50218ee5605d8a66cbd940e
Michael Markevich <mmarkevich@dhis2.org>
2023-05-10 23:48:04 +0200

    Adding DB configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,29 @@
 
 # Common variables
 DHIS2_EMAIL="security@dhis2.org"
+DHIS2_USER="dhis2"
+DHIS2_DB="empty"
+DHIS2_DBUSER=$DHIS2_USER
+DHIS2_DBPASS=$DHIS2_USER
+
+# Additional variables
+TMP_DIR="/tmp"
 
 # Update the environment
 apt-get -qq update
 apt-get -yqq dist-upgrade
 
-# Fetch the FQDN
+# Try to fetch the FQDN
 apt-get install -yqq curl jq
-DHIS2_SPECIMEN_HOST=`curl -s http://169.254.169.254/openstack/latest/meta_data.json | jq -j .name`
+DHIS2_SPECIMEN_HOST=$(curl -s --connect-timeout 10 http://169.254.169.254/openstack/latest/meta_data.json | jq -j .name)
 
 # Set the FQDN
-echo -e "127.0.0.1\tlocalhost\n127.0.1.1\t$DHIS2_SPECIMEN_HOST `hostname`\n\n::1\tlocalhost ip6-localhost ip6-loopback\nff02::1\tip6-allnodes\nff02::2\tip6-allrouters" > /etc/hosts
- 
+if [ -n "$DHIS2_SPECIMEN_HOST" ]; then
+    echo -e "127.0.0.1\tlocalhost\n127.0.1.1\t$DHIS2_SPECIMEN_HOST $(hostname)\n\n::1\tlocalhost ip6-localhost ip6-loopback\nff02::1\tip6-allnodes\nff02::2\tip6-allrouters" > /etc/hosts
+fi
+
 # We install and configure a default OS environment
-apt-get install -yqq gettext-base git sed software-properties-common unattended-upgrades
+apt-get install -yqq gettext-base git sed software-properties-common sudo unattended-upgrades
 
 # We install and configure default services
 apt-get install -yqq certbot default-jdk-headless default-jre-headless nginx postgresql postgresql-client postgresql-*-postgis-3 tomcat9 tomcat9-admin tomcat9-user
@@ -27,3 +36,11 @@ apt-get install net-tools testinfra
 mkdir -p /etc/ssh/sshd_config.d
 echo "PasswordAuthentication no" > /etc/ssh/sshd_config.d/no_password.conf
 systemctl reload ssh
+
+# Configure the database
+sudo -D $TMP_DIR -u postgres createuser -SDR $DHIS2_DBUSER
+sudo -D $TMP_DIR -u postgres createdb -O $DHIS2_DBUSER $DHIS2_DB
+sudo -D $TMP_DIR -u postgres psql -c "ALTER USER $DHIS2_DBUSER PASSWORD '$DHIS2_DBPASS';"
+sudo -D $TMP_DIR -u postgres psql -c "create extension postgis;" $DHIS2_DB
+sudo -D $TMP_DIR -u postgres psql -c "create extension btree_gin;" $DHIS2_DB
+sudo -D $TMP_DIR -u postgres psql -c "create extension pg_trgm;" $DHIS2_DB
