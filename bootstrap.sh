9a11fb10e334e237a675beedbef3705a3101fb06
Michael Markevich <mmarkevich@dhis2.org>
2023-06-12 23:43:47 +0200

    Linting the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,11 +17,11 @@ apt-get install -yqq coreutis curl gettext-base git jq sudo
 
 # Set additional variables
 DHIS2_TMP=$(mktemp -d)
-DHIS2_SRC=$DHIS_TMP/dhis2-specimen
+DHIS2_SRC=$DHIS2_TMP/dhis2-specimen
 
 # Clone the directory with templates
 # TODO: check that the repository is not empty
-git clone https://github.com/dhis2-sre/dhis2-specimen.git $DHIS2_SRC
+git clone https://github.com/dhis2-sre/dhis2-specimen.git "$DHIS2_SRC"
 
 # Fetch hostname and FQDN
 # TODO: check that the left part matches
@@ -32,7 +32,7 @@ export DHIS2_FQDN=$(curl -s --connect-timeout 10 http://169.254.169.254/openstac
 # TODO: handle missing FQDN after "if"
 if [ -n "$DHIS2_FQDN" ]; then
     echo "Setting hostname to '$DHIS2_FQDN'"
-    cat $DHIS2_SRC/templates/etc/hosts | envsubst "$(printf '${%s} ' ${!DHIS2_*})" > /etc/hosts
+    cat "$DHIS2_SRC/templates/etc/hosts" | envsubst "$(printf '${%s} ' ${!DHIS2_*})" > /etc/hosts
 fi
 
 # We install and configure a default OS environment and tools
@@ -40,20 +40,20 @@ apt-get install -yqq net-tools software-properties-common testinfra
 
 # We install and configure default services
 apt-get install -yqq certbot nginx
-mkdir -p /var/www/$DHIS2_FQDN
-certbot certonly --quiet --noninteractive --agree-tos -m $DHIS2_EMAIL --webroot -w /var/www/html --post-hook "systemctl reload nginx" $DHIS2_FQDN
-cat $DHIS2_SRC/templates/etc/nginx/sites-available/specimen | envsubst "$(printf '${%s} ' ${!DHIS2_*})" > etc/nginx/sites-available/$DHIS2_FQDN
-ln -s /etc/nginx/sites-available/$DHIS_FQDN /etc/nginx/sites-enabled/$DHIS_FQDN
+mkdir -p "/var/www/$DHIS2_FQDN"
+certbot certonly --quiet --noninteractive --agree-tos -m $DHIS2_EMAIL --webroot -w /var/www/html --post-hook "systemctl reload nginx" "$DHIS2_FQDN"
+cat "$DHIS2_SRC/templates/etc/nginx/sites-available/specimen" | envsubst "$(printf '${%s} ' ${!DHIS2_*})" > "/etc/nginx/sites-available/$DHIS2_FQDN"
+ln -s "/etc/nginx/sites-available/$DHIS2_FQDN" "/etc/nginx/sites-enabled/$DHIS2_FQDN"
 systemctl reload nginx
 
 # Create the DHIS2 database
 apt-get install -yqq postgresql postgresql-client postgresql-*-postgis-3
-sudo -D $DHIS2_TMP -u postgres createuser -SDR $DHIS2_DBUSER
-sudo -D $DHIS2_TMP -u postgres createdb -O $DHIS2_DBUSER $DHIS2_DB
-sudo -D $DHIS2_TMP -u postgres psql -c "ALTER USER $DHIS2_DBUSER PASSWORD '$DHIS2_DBPASS';"
-sudo -D $DHIS2_TMP -u postgres psql -c "create extension postgis;" $DHIS2_DB
-sudo -D $DHIS2_TMP -u postgres psql -c "create extension btree_gin;" $DHIS2_DB
-sudo -D $DHIS2_TMP -u postgres psql -c "create extension pg_trgm;" $DHIS2_DB
+sudo -D "$DHIS2_TMP" -u postgres createuser -SDR $DHIS2_DBUSER
+sudo -D "$DHIS2_TMP" -u postgres createdb -O $DHIS2_DBUSER $DHIS2_DB
+sudo -D "$DHIS2_TMP" -u postgres psql -c "ALTER USER $DHIS2_DBUSER PASSWORD '$DHIS2_DBPASS';"
+sudo -D "$DHIS2_TMP" -u postgres psql -c "create extension postgis;" $DHIS2_DB
+sudo -D "$DHIS2_TMP" -u postgres psql -c "create extension btree_gin;" $DHIS2_DB
+sudo -D "$DHIS2_TMP" -u postgres psql -c "create extension pg_trgm;" $DHIS2_DB
 
 apt-get install -yqq default-jdk-headless default-jre-headless 
 apt-get install -yqq tomcat9 tomcat9-user
